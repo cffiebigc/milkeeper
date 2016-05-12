d4501b4175e08694f6121fd354480aaa40b96118
Carlos Fiebig <cffiebigc@gmail.com>
2016-05-12 10:46:11 -0300

    Add milkeeper funtionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Botkit = require('botkit')
 
+var milkDate = new Date('1990-01-01');
+
 // Expect a SLACK_TOKEN environment variable
 var slackToken = process.env.SLACK_TOKEN
 if (!slackToken) {
@@ -22,9 +24,27 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-})
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'cow',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello, if you need help just type @milkeeper `help` / `ayuda`');
+        }
+    });
+});
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
   bot.reply(message, 'Hello.')
@@ -35,33 +55,41 @@ controller.hears('.*', ['mention'], function (bot, message) {
   bot.reply(message, 'You really do care about me. :heart:')
 })
 
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
+controller.hears(['help', 'ayuda'], 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, 'Take it easy, usage:\n'+ 
+        '-@milkeeper `i changed the milk today` / `cambie la leche hoy`\n'+
+        '-@milkeeper `i changed the milk at [yyyy-mm-dd]` / `cambie la leche el [yyyy-mm-dd]`\n'+
+        '-@milkeeper `how is the milk?` / `como esta la leche?`  or just `milk` / `leche`\n');
+});
 
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
+controller.hears(['changed the milk at (.*)', 'cambie la leche el (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+    milkDate = new Date(message.match[1]);
+    var diff =  Math.floor(( Date.parse(new Date()) - Date.parse(milkDate) ) / 86400000);
+    if (milkDate == 'Invalid Date')
+        bot.reply(message, 'Hey! please give me a valid date ');
+    else if (diff < 0)
+        bot.reply(message, 'Hey! nice try smartass! today is ' + new Date().toDateString());
+    else{
+        milkDate.setTime(milkDate.getTime() + milkDate.getTimezoneOffset()*60*1000);    
+        bot.reply(message, 'Great. Milk changed at ' + milkDate.toDateString());
+    }
+});
 
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
+controller.hears(['changed the milk today', 'cambie la leche hoy'], 'direct_message,direct_mention,mention', function(bot, message) {
+    milkDate = new Date();
+    bot.reply(message, 'Great. Milk changed at ' + milkDate.toDateString());
+});
+
+controller.hears(['milk', 'leche'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var diff =  Math.floor(( Date.parse(new Date()) - Date.parse(milkDate) ) / 86400000);
+    milkDate.setTime(milkDate.getTime() + milkDate.getTimezoneOffset()*60*1000);
+    var days = 'days'
+    if (diff == 1)
+        days = 'day'
+    bot.reply(message, 'Last Milk change was at ' + milkDate.toDateString() + ' has been in the fridge for ' + diff + ' '+ days);
+    if (diff > 3)
+        bot.reply(message, 'Maybe you should change it :wink:') ;
+});
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
